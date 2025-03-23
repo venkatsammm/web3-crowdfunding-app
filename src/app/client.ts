bdeb2cb0f315d3169dc0825f33ee0fd488232990
JEEVEEKA <jeeveeka.ps@kalvium.community>
2025-03-23 09:35:11 +0530

    done

--- src/app/client.ts
+++ src/app/client.ts
@@ -1,7 +1,5 @@
 import { createThirdwebClient } from "thirdweb";
 
-// Replace this with your client ID string
-// refer to https://portal.thirdweb.com/typescript/v5/client on how to get a client ID
 const clientId = "d4b41d8b61c3db10f396c9036a945108";
 
 if (!clientId) {
